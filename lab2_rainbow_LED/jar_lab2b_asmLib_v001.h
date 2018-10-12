f4a1d4d0f45a58c803164da97fcef06a68022119
jarga001 <38598305+jarga001@users.noreply.github.com>
2018-10-11 20:03:15 -0500

    color_code_header

---
+++ lab2_rainbow_LED/jar_lab2b_asmLib_v001.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   jar_lab2b_asmLib_v001.h
+ * Author: baska
+ *
+ * Created on February 7, 2018, 11:53 AM
+ */
+
+#ifndef JAR_LAB2B_ASMLIB_V001_H
+#define	JAR_LAB2B_ASMLIB_V001_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+void jar_wait_50microSec(void);
+void write_0(void);
+void write_1(void);
+void wait1_msec(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* JAR_LAB2B_ASMLIB_V001_H */
+
